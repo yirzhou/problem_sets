643677fc54a8d58edc5621ff06867f86767f092b
Yiren Zhou <yiren.chow@gmail.com>
2020-04-13 22:21:59 -0400

    added kth last node

---
+++ set_1/kth_last_node.py
@@ -0,0 +1,66 @@
+import unittest
+
+
+def kth_to_last_node(k, head):
+    if k == 0: raise ValueError('invalid input')
+    slow, fast = head, head
+    for _ in range(k-1): 
+        try:
+            fast = fast.next
+        except Exception:
+            raise Exception('list is not that long')
+    while fast.next:
+        slow = slow.next
+        fast = fast.next
+
+    return slow
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    class LinkedListNode(object):
+
+        def __init__(self, value, next=None):
+            self.value = value
+            self.next  = next
+
+        def get_values(self):
+            node = self
+            values = []
+            while node is not None:
+                values.append(node.value)
+                node = node.next
+            return values
+
+    def setUp(self):
+        self.fourth = Test.LinkedListNode(4)
+        self.third = Test.LinkedListNode(3, self.fourth)
+        self.second = Test.LinkedListNode(2, self.third)
+        self.first = Test.LinkedListNode(1, self.second)
+
+    def test_first_to_last_node(self):
+        actual = kth_to_last_node(1, self.first)
+        expected = self.fourth
+        self.assertEqual(actual, expected)
+
+    def test_second_to_last_node(self):
+        actual = kth_to_last_node(2, self.first)
+        expected = self.third
+        self.assertEqual(actual, expected)
+
+    def test_first_node(self):
+        actual = kth_to_last_node(4, self.first)
+        expected = self.first
+        self.assertEqual(actual, expected)
+
+    def test_k_greater_than_linked_list_length(self):
+        with self.assertRaises(Exception):
+            kth_to_last_node(5, self.first)
+
+    def test_k_is_zero(self):
+        with self.assertRaises(Exception):
+            kth_to_last_node(0, self.first)
+
+
+unittest.main(verbosity=2)
