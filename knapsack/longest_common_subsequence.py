168ebf4c2ef77b6d993b7ebaf84db3c462eae03e
Yiren Zhou <yiren.chow@gmail.com>
2020-04-26 14:43:04 -0400

    daily challenge 26

---
+++ knapsack/longest_common_subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longest_common_subsequence(self, text1: str, text2: str) -> int:
+        '''This is similar to the knapsack problem.
+        Note the way of allocating a multi-dimensional list:
+        - dp = [[0 for col in range(len(text2)+1)] for row in range(len(text1)+1)]
+        1. Time: O(m*n), where m and n are the lengths of strings
+        2. Space: O(m*n)
+        '''
+        n,m=len(text1),len(text2)
+        dp = [[0 for col in range(len(text2)+1)] for row in range(len(text1)+1)]
+        for i, c in enumerate(text1):
+            for j, d in enumerate(text2):
+                dp[i + 1][j + 1] = 1 + dp[i][j] if c == d else max(dp[i][j + 1], dp[i + 1][j])
+        return dp[-1][-1]
+
+print(Solution().longestCommonSubsequence("pmjghexybyrgzczy", "hafcdqbgncrcbihkd"))
