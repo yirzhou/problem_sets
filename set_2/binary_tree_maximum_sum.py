8328a5dbea55db0cfe89c90e3b0735b77c7251bc
Yiren Zhou <yiren.chow@gmail.com>
2020-04-29 12:20:29 -0400

    iterative daily challenge 29

--- set_2/binary_tree_maximum_sum.py
+++ set_2/binary_tree_maximum_sum.py
@@ -6,6 +6,39 @@ class Solution:
     The path must contain at least one node and does not need to go through the root.
     '''
     def __init__(self): self.cur_max = float('-inf')
+
+    def max_path_sum_iterative(self, root):
+        '''The iterative approach using similar logic.
+        We are still doing a post-order traversal.
+
+        - Time: O(N)
+        - Space: O(N) but not limited by the call stack
+        '''
+        if not root: return 0
+        cur_max = float('-inf')
+        stack1, stack2 = [(root, None, -1)], []
+        left, right = dict(), dict()
+        
+        # constructing post order traversal stack 
+        while len(stack1):
+            (node, parent, prop) = stack1.pop()
+            stack2.append((node, parent, prop))
+            if node.left: stack1.append((node.left, node, 0))
+            if node.right: stack1.append((node.right, node, 1))
+                
+        while len(stack2):
+            (node, parent, prop) = stack2.pop()
+            
+            left_max = left.get(node, 0)
+            right_max = right.get(node, 0)
+            
+            cur_max = max(cur_max, node.val, node.val+left_max, node.val+right_max, node.val+left_max+right_max)
+            
+            if parent:
+                # left subtree
+                if not prop: left[parent] = max(node.val, node.val+right_max, node.val+left_max)
+                else: right[parent] = max(node.val, node.val+right_max, node.val+left_max)
+        return cur_max 
     
     def max_path_sum(self, root: TreeNode) -> int:
         self.cur_max = float('-inf')
