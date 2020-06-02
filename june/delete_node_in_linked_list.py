20ee1e5e5bde17517800208d06f9d55cbed4ad96
Larry Zhou <yiren.zhou@uwaterloo.ca>
2020-06-02 13:16:48 -0400

    daily challenge 2

---
+++ june/delete_node_in_linked_list.py
@@ -0,0 +1,11 @@
+class Solution:
+    """#237. Delete Node in a Linked List in Place."""
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        next_node = node.next
+        node.val = next_node.val
+        node.next = next_node.next
+        
