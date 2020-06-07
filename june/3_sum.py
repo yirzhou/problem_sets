b4e277e9a85d6cf01e09f10dca473d1c9f0be8cd
Larry Zhou <yiren.zhou@uwaterloo.ca>
2020-06-07 10:27:34 -0400

    3 sum

---
+++ june/3_sum.py
@@ -0,0 +1,37 @@
+class Solution:
+    """#15. Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+    Find all unique triplets in the array which gives the sum of zero."""
+    def threeSum(self, nums):
+        """Traditional solution.
+        - Time: O(N^2)
+        - Space: O(N)
+        """
+        results = set()
+        results_list = []
+        visited = set()
+        for index, num in enumerate(nums):
+            if num in visited: continue
+            visited.add(num)
+            partial = self.two_sum(nums, 0-num, index+1)
+            for (a,b) in partial:
+                result_sorted = sorted([num,a,b])
+                result_tuple = tuple(result_sorted)
+                if result_tuple not in results: 
+                    results_list.append(result_sorted)
+                    results.add(result_tuple)
+        return results_list
+        
+        
+    def two_sum(self, nums, target, begin):
+        if begin > len(nums)-1: return []
+        cache = {}
+        results = []
+        visited = set()
+        for i in range(begin, len(nums)):
+            if target-nums[i] in cache:
+                potential_result = (min(nums[i], target-nums[i]),max(nums[i], target-nums[i]))
+                if potential_result not in visited:
+                    results.append(potential_result)
+                    visited.add(potential_result)
+            cache[nums[i]] = i
+        return results
