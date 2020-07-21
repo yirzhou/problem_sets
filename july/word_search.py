2c42e8346f82223359980885b35cdf5c212ab2a4
Yiren Zhou <yiren.chow@gmail.com>
2020-07-21 10:28:22 -0400

    d c 21

---
+++ july/word_search.py
@@ -0,0 +1,36 @@
+class Solution:
+    """#79. Given a 2D board and a word, find if the word exists in the grid.
+
+    The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. 
+
+    The same letter cell may not be used more than once.
+    """
+    def exist(self, board, word):
+        if not word:
+            return True
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.exist_helper(board, word, i, j):
+                    return True
+        return False
+                    
+    def exist_helper(self, board, word, i, j):
+        if board[i][j] == word[0]:
+            if not word[1:]:
+                return True
+            board[i][j] = " " # indicate used cell
+            # check all adjacent cells
+            if i > 0 and self.exist_helper(board, word[1:], i-1, j):
+                return True
+            if i < len(board)-1 and self.exist_helper(board, word[1:], i+1, j):
+                return True
+            if j > 0 and self.exist_helper(board, word[1:], i, j-1):
+                return True
+            if j < len(board[0])-1 and self.exist_helper(board, word[1:], i, j+1):
+                return True
+            board[i][j] = word[0] # update the cell to its original value
+            return False
+        else:
+            return False
